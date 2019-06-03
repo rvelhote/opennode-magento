5a430cc9757b7e9085a45794f069354e02d86c9c
Ricardo Velhote <rvelhote@gmail.com>
2019-06-03 20:55:15 +0100

    Update the sample install script

--- shell/install.bash
+++ shell/install.bash
@@ -8,29 +8,40 @@ DATABASE="magentodb"
 URL="http://development.opennode.co"
 
 # Install n98-magerun
+echo "Downloading n98-magerun"
 php -r "copy('https://files.magerun.net/n98-magerun.phar', 'bin/n98-magerun.phar');"
 chmod +x bin/n98-magerun.phar
-echo "INSTALLED n98-magerun to the BIN directory"
+echo "Installed n98-magerun to the BIN directory"
 
 rm -f src/app/etc/local.xml
 
+echo "Dropping any existing databases named ${DATABASE}"
 mysql -u$USER -p$PASS -h$HOST -e "drop database ${DATABASE};"
+
+echo "Create a new database ${DATABASE}"
 mysql -u$USER -p$PASS -h$HOST -e "create database ${DATABASE};"
 
+echo "Importing the Magento 1.9 sample database"
 mysql -u$USER -p$PASS -h$HOST $DATABASE < data/magento-sample-data/magento_sample_data_for_1.9.2.4.sql
-echo "IMPORTED the SAMPLE DATABASE"
+echo "Imported the the sample database"
 
+echo "Copy all sample folders"
 cp -R data/magento-sample-data/media src
 cp -R data/magento-sample-data/skin src
-echo "COPIED all SAMPLE folders for the Magento Demo Store"
+echo "Finished copying all sample folders for the Magento Demo Store"
 
+echo "Installing Magento 1.9.4.1"
 bin/n98-magerun.phar install --dbHost=$HOST --dbUser=$USER --dbPass=$PASS --forceUseDb --dbName=$DATABASE --useDefaultConfigParams=yes --noDownload --installationFolder="src" --baseUrl=$URL
-echo "Installed the SAMPLE DATABASE"
+echo "Installed Magento 1.9.4.1"
 
+# Discount everything
 php src/shell/opennode.php
-echo "Installed BITCOIN discount rule so that prices can be lower all over the website"
+echo "Installed a discount rule so that prices can be lower all over the website"
 
-echo "Backend: ${URL}/admin"
-echo "User: admin"
-echo "Pass: password123"
-echo "Add your OpenNode Development Key in System » Configuration » Payment Methods » OpenNode Bitcoin"
+./bin/n98-magerun.phar --root-dir=src customer:change-password janedoe@example.com password123
+./bin/n98-magerun.phar --root-dir=src config:set carriers/flatrate/active 0
+./bin/n98-magerun.phar --root-dir=src config:set carriers/freeshipping/free_shipping_subtotal 0
+
+./bin/n98-magerun.phar --root-dir=src cache:clean
+
+echo "All Done!"
