13dd49e7bee8db6bbebdd2d1553aed5b3c7ff3c8
Ricardo Velhote <ricardo@caretobeauty.com>
2020-04-19 13:00:59 +0100

    No need to build the docker images every time the install is executed

---
+++ shell/docker.bash
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# Install Magento + Sample Data
+RED="\033[0;31m"
+YELLOW="\033[0;33m"
+GREEN="\033[0;32m"
+NONE="\033[0m"
+
+USER="root"
+PASS="control"
+HOST="db"
+DATABASE="magentodb"
+URL="http://development.opennode.co"
+
+echo -e "${RED}"
+cat <<EOF
+ ______  __________________ _______  _______ _________ _
+(  ___ \ \__   __/\__   __/(  ____ \(  ___  )\__   __/( (    /|
+| (   ) )   ) (      ) (   | (    \/| (   ) |   ) (   |  \  ( |
+| (__/ /    | |      | |   | |      | |   | |   | |   |   \ | |
+|  __ (     | |      | |   | |      | |   | |   | |   | (\ \) |
+| (  \ \    | |      | |   | |      | |   | |   | |   | | \   |
+| )___) )___) (___   | |   | (____/\| (___) |___) (___| )  \  |
+|/ \___/ \_______/   )_(   (_______/(_______)\_______/|/    )_)
+EOF
+
+echo -e "${NONE}"
+
+echo -e "${YELLOW}Bulding Nginx...${NONE}"
+docker build -t magento-nginx docker/nginx/ > /dev/null
+
+echo -e "${YELLOW}Building PHP...${NONE}"
+docker build -t magento-php docker/php/ > /dev/null
+
+echo -e "${GREEN}All done!${NONE}"
